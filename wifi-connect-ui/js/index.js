41838ae3c65564140629b836c422f873653b3bce
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2018-02-10 13:51:33 +0100

    Use a list group instead of a select

--- wifi-connect-ui/js/index.js
+++ wifi-connect-ui/js/index.js
@@ -4,9 +4,8 @@ $(function () {
             $('.js-before-submit').hide();
             $('.no-networks-message').removeAttr('hidden');
         } else {
-            $(".js-ssid-select").append('<option value="">Maak een keuze</option>');
             $.each(JSON.parse(data), function (i, val) {
-                $(".js-ssid-select").append($('<option>').attr('val', val).text(val));
+                $(".js-ssid-select").append('<button class="list-group-item list-group-item-action js-select-wifi-network">' + val + '</button>');
             });
         }
     });
@@ -20,5 +19,12 @@ $(function () {
             $('.js-before-submit').hide();
             $('.submit-message').removeAttr('hidden');
         });
+    });
+
+    $(document).on('click', '.js-select-wifi-network', function () {
+        $('input[name="ssid"]').val($(this).html());
+        $('.js-select-wifi-network').removeClass('active');
+        $(this).addClass('active');
+        return false;
     });
 });
